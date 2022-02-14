d43faee13c517cbaf57d7fc2f4db464e94e2fbbd
antalsz96 <91131342+antalsz96@users.noreply.github.com>
2022-02-14 15:39:53 +0100

    Add files via upload

--- checkerboard.py
+++ checkerboard.py
@@ -3,21 +3,30 @@ mon = monitors.Monitor("testMonitor", distance=90)
 win = visual.Window(monitor=mon, fullscr=True, viewPos=(-11.45, -9), units="deg")
 ISI = core.StaticPeriod(screenHz=85)
 
+cross_hor = visual.Line(win=win, start=[11.4,9.5], end=[11.6,9.5], lineWidth=2, lineColor="red", units="deg")
+cross_ver = visual.Line(win=win, start=[11.5,9.4], end=[11.5,9.6], lineWidth=2, lineColor="red", units="deg")
+
+
 g1 = visual.GratingStim(win=win, tex="sqrXsqr", units="deg", pos=(0,0), size=(48,38), sf=0.5)
 g2 = visual.GratingStim(win=win, tex="sqrXsqr", units="deg", pos=(0,0), size=(48,38), sf=0.5, contrast=-1)
 
-timer = core.CountdownTimer(15)
-while timer.getTime() > 0:
-    # frame 1
-    ISI.start(0.5)
-    g1.draw()
-    win.flip()
-    ISI.complete()
+# timer = core.CountdownTimer(15)
+# while timer.getTime() > 0:
+#     # frame 1
+#     ISI.start(0.5)
+#     g1.draw()
+#     win.flip()
+#     ISI.complete()
     
-    # frame 2
-    ISI.start(0.5)
-    g2.draw()
-    win.flip()
-    ISI.complete()
+#     # frame 2
+#     ISI.start(0.5)
+#     g2.draw()
+#     win.flip()
+#     ISI.complete()
 
-    
+g1.draw()
+cross_ver.draw()
+cross_hor.draw()
+win.flip()
+event.waitKeys()
+win.close()
